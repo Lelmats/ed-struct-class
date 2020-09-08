f40e08a695e623bc92c175a329c76eedcbcb1b12
Lelmats <maxalejandrorh@hotmail.com>
2020-09-08 08:23:56 -0700

    3

--- src/main.cc
+++ src/main.cc
@@ -5,7 +5,16 @@
 int main()
 {
 
-    Hero* hero1{new Hero(5, 3, 1, 2, 3)};
+    Hero* hero1{new Hero(5, 3, 1, 2, 3, 1, new std::string[4]{{"fire ball"}, {"fenix tail"}, {"war growl"}, {"fly"}})};
+    std::cout << "Hero Skills:" << std::endl;
+
+    for(int i{}; i < 4; i++)
+    {
+        std::cout << *(hero1->GetSkills() + i) << std::endl;
+    }
+    std::cout << "level: " << hero1->GetLevel() << std::endl;
+    //std::cout << hero1. << std::endl;
+
     /*Character* character1{new Character(5, 3, 1, 2, 3)};
 
     std::cout << "Character info:" << std::endl;
@@ -18,7 +27,7 @@ int main()
     character1->GetDamage(2);
     std::cout << "Health: " << character1->GetHealth() << std::endl;
     std::cout << "Lore: : " << character1->GetLore() << std::endl;
-    delete &character1; */
+    delete &character1;*/
 
     std::cin.get();
     return 0;
